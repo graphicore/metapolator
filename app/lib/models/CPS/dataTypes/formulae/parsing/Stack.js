af78947c41b58717f523f68feef60b696114e881
Lasse Fister <commander@graphicore.de>
2014-09-05 15:59:11 +0200

    inject getAPI into call to operator execute, jsinht fixes

--- app/lib/models/CPS/dataTypes/formulae/parsing/Stack.js
+++ app/lib/models/CPS/dataTypes/formulae/parsing/Stack.js
@@ -1,7 +1,9 @@
 define([
     'metapolator/errors'
+  , './_ValueToken'
 ], function(
     errors
+  , _ValueToken
 ) {
     "use strict";
 
@@ -16,16 +18,16 @@ define([
 
     var _p = Stack.prototype;
     _p.toString = function() {
-        return this._stack.join('|')
-    }
+        return this._stack.join('|');
+    };
 
     Object.defineProperty(_p, 'items', {
-        get: function(){ return this._stack.slice() }
+        get: function(){ return this._stack.slice(); }
     });
 
     _p._check = function(stack) {
-        var stackLen = 0;
-        for(var i=0; i<stack.length;i++) {
+        var i=0, stackLen = 0;
+        for(; i<stack.length;i++) {
             stackLen -= stack[i].consumes;
             if(stackLen < 0)
                 throw new CPSFormulaError('Stack underflow at ('+i+') a '
@@ -35,29 +37,32 @@ define([
             stackLen += stack[i].ejects;
         }
         if(stackLen > 1)
-            throw new CPSFormulaError('Stack to crowded. A stack must '
+            throw new CPSFormulaError('Stack too crowded. A stack must '
                         +'eventually resolve to 1 item, the result. This '
                         +'stack has still ' + stackLen + ' items: '
                         + stack.join('|'));
-    }
+    };
 
-    _p.execute = function() {
+    _p.execute = function(getApi) {
         var commands = this._stack.slice()
           , stack = []
+          , args
           , i = 0
           ;
         for(;i<commands.length;i++) {
-            if(commands[i] instanceof Value)
+            if(commands[i] instanceof _ValueToken)
                 stack.push(commands[i]);
             else {
-                stack.push(
-                    commands[i].execute.apply(commands[i],
-                                stack.splice(-commands[i].consumes))
-                );
+                args = [getApi];
+                Array.prototype.push.apply(args
+                                , stack.splice(-commands[i].consumes));
+                // commands always return only one element currently
+                stack.push( commands[i].execute.apply(commands[i], args) );
             }
         }
+        // stack.length should be 1 at this point
         return stack.pop();
-    }
+    };
 
     return Stack;
-}
+});
